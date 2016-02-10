4b634a3941af620226a246934cd16899ecab9601
Simon Novelly <Simon Novelly>
2016-02-09 20:29:57 -0600

    updating put_values.php needs testing.

--- powercc_hmi/power/put_values.php
+++ powercc_hmi/power/put_values.php
@@ -1,9 +1,22 @@
 <?php
 
+$systems = array(
+    "gen1_breaker" => "gen1_breaker",
+    "gen1_generation" => "gen1_generation",
+    "relay1_breaker" => "relay1_breaker",
+    "gen2_breaker" => "gen2_breaker",
+    "gen2_generation" => "gen2_generation",
+    "relay2_breaker" => "relay2_breaker"
+)
+
+
 $var = $_GET['var'];
 $value = $_GET['value'];
 
+$sys = $systems[$var];
+$val = int($value);
 
-exec("python py-bin/opc_put_values.py ".$var." ".$value." 2>&1", $output, $return_var);
+
+exec("python py-bin/opc_put_values.py ".$sys." ".$val." 2>&1", $output, $return_var);
 
 ?>
