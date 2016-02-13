8c841a27463bae16be54e03bb2423a40d08e49f7
Simon Novelly <Simon Novelly>
2016-02-13 07:21:16 -0600

    new update

--- wtc_updated/lib/Process.py
+++ wtc_updated/lib/Process.py
@@ -8,12 +8,12 @@ from PyOPC.OPCContainers import *
 		source = str(failValue) + logic + str(cur)
 		node = ast.parse(source, mode='eval')
 		return eval(compile(node, '<string>', mode='eval'))
-	return CheckFailed'''
+	return CheckFailed
     
 def create_state_compare(failValue):
 	def CheckFailed(cur):
 		return failValue == cur
-	return CheckFailed
+	return CheckFailed'''
 
 class ProcessObject:
 	def __init__(self, name, failValue, depends, next_list, cur=0, state=True):
@@ -24,19 +24,22 @@ class ProcessObject:
 		self.depends = []
 		self.failValue = failValue
 		self.state = state
-		self.checkFailed = create_state_compare(self.failValue)
+		#self.checkFailed = create_state_compare(self.failValue)
 		self.cur = cur
 		self.dirty = False
 
 	def UpdateState(self, cur):
 		self.cur = cur
-		if not self.checkFailed(cur):
+		if not self.CheckFailed():
 			self.state = True
 		else:
 			self.state = False
 
 	def Online(self):
 		return self.state
+        
+    def CheckFailed():
+        return self.failValue == self.cur
 
 
 
